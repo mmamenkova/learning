0e134fc3983aaa7578d24d9835796e25bfd8051c
mmamenkova <mpodrugina@gmail.com>
2016-11-29 00:48:42 +0300

    Update task2D.js

--- task2D.js
+++ task2D.js
@@ -4,10 +4,53 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+function HSLtoRGB(hslcolor) {
+  const notspace = hslcolor.replace(/%20/g,'');
+  const re = new RegExp('^hsl\\((\\d{1,3}),(\\d{1,3})([%])?,(\\d{1,3})([%])?\\)');
+  const reer = new RegExp('-');
+  const error = notspace.match(reer);
+  const recolor = notspace.match(re);
+
+  if (error || recolor[3] == null || recolor[5] == null) {
+    return ('Invalid color');
+  } else {
+    const recolor = notspace.match(re);
+    if (recolor[1]/360 > 1 || recolor[2]/100 > 1 || recolor[3]/100 > 1) {
+      return ('Invalid color');
+    } else {
+      const h = recolor[1]/360;
+      const s = recolor[2]/100;
+      const l = recolor[4]/100;
+      var r, g, b;
+
+      if(s === 0){
+        r = g = b = l;
+      } else {
+        var hue2rgb = function hue2rgb(p, q, t){
+          if(t < 0) t += 1;
+          if(t > 1) t -= 1;
+          if(t < 1/6) return p + (q - p) * 6 * t;
+          if(t < 1/2) return q;
+          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+          return p;
+        }
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+      }
+
+      const rgbcolor = [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+      return ('rgb(' + rgbcolor + ')');
+    }
+  }
+
+};
+
 function RGBtoHEX(rgbcolor) {
   const re = new RegExp('^rgb\\((\\d{1,3}[%]?),(\\d{1,3}[%]?),(\\d{1,3}[%]?)\\)');
   const recolor = rgbcolor.match(re);
-  console.log(recolor);
   if (recolor) {
     let color = '';
     for (var i = 1; i <= 3; i++) {
@@ -59,17 +102,29 @@ function HEXcolor(hexcolor) {
 app.get('/2D', (req, res) => {
   if (req.query.color == null || req.query.color == '') {
     res.send('Invalid color');
+    console.log('Invalid color');
   } else {
     const reqcolor = req.query.color.toLowerCase();
     const notspace = reqcolor.replace(/\s/g,'');
     const re_rgb = new RegExp('^rgb');
+    const re_hsl = new RegExp('^hsl');
     const rgb_color = notspace.match(re_rgb);
+    const hsl_color = notspace.match(re_hsl);
     if (rgb_color) {
       const color = RGBtoHEX(notspace);
       res.send(color);
+      console.log(color);
     } else {
-      const color = HEXcolor(notspace);
-      res.send(color); 
+      if (hsl_color) {
+        const rgbcolor = HSLtoRGB(notspace);
+        const color = RGBtoHEX(rgbcolor);
+        res.send(color);
+        console.log(rgbcolor);
+      } else {
+        const color = HEXcolor(notspace);
+        console.log(color);
+        res.send(color)
+      }
     }
   }
 });
